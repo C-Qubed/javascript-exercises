bc6a93950ca909c1c6f66ac2832a062659c8df43
Cameron <cameronpeck317@gmail.com>
2022-07-28 09:28:58 -0400

    complete exercise 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+  let result = ''
+  if (repeatCount < 0) {
+    return 'ERROR'
+  }
+  while (repeatCount > 0) {
+    repeatCount--
+    result += string      
+  }
+  return result
 };
 
 // Do not edit below this line
