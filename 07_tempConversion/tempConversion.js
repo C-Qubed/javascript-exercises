6e0b4116cce8a9450960d765a297d48d80019798
Cameron <cameronpeck317@gmail.com>
2022-07-28 11:24:07 -0400

    solutions for exercises 06 & 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let num = ((temp - 32) * 5 / 9)
+  let roundedNum = Math.round(num * 10) / 10
+  return roundedNum
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let num = ((temp * 1.8) + 32)
+  let roundedNum = Math.round(num * 10) / 10
+  return roundedNum
 };
 
 // Do not edit below this line
