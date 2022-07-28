dfaf59b9583965de11d8eb411163708ddc6179b7
Cameron <cameronpeck317@gmail.com>
2022-07-28 10:21:13 -0400

    complete exercise 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  // check if string is empty
+  if (string.length === 0) {
+    return ''
+  }
+  // split string into individual letters in an array
+  const splitString = string.split('')
+  // use reverse method to reverse order of letters
+  const reverseArray = splitString.reverse()
+  // use join method to rejoin elements into string
+  const reverseString = reverseArray.join('')
+  // return reversed string
+  return reverseString
 };
+
+console.log(reverseString(''))
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const NewArray = [];
+  array.forEach(el => {
+    if (!args.includes(el)) {
+        NewArray.push(el);
+    }
+  });
+  return NewArray
 };
 
 // Do not edit below this line
