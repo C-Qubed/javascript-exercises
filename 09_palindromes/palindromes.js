6c77aa371071756e4a831913ba2237c70169c446
Cameron <cameronpeck317@gmail.com>
2022-08-04 13:22:15 -0400

    complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (word) {
+    // remove non-letters and spaces from word
+    let removedSpaces = word.replace(/ /g, "");
+    let inputWord = removedSpaces.replace(/[^a-zA-Z ]/g, "").toLowerCase();
+    console.log(inputWord)
+    //split the word into an array
+    let reverseWordArr = inputWord.split("");
+    // reverse the array
+    let reverseArray = reverseWordArr.reverse();
+    // rejoin the array elements into a string
+    let reversedWord = reverseArray.join("");
+  	
+    // return t/f comparing input word to reverse word
+    return inputWord === reversedWord;
+}
 // Do not edit below this line
 module.exports = palindromes;
