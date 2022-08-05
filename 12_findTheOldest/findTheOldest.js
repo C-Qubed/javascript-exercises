08e89fded86f463be2564ec2de3611c831084844
Cameron <cameronpeck317@gmail.com>
2022-08-05 11:40:57 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    // check input objects for currenly alive people
+    array.forEach(person => {
+    if (person.yearOfDeath === undefined) {
+        // get current year
+        const currentYear = new Date().getFullYear();
+        // set death year as current year for age calc later
+        person.yearOfDeath = currentYear;
+    };
+  });
+  // map current ages to new array
+  const ageMap = array.map(person => person.yearOfDeath - person.yearOfBirth);
+  const max = Math.max(...ageMap);
+  const oldestIndex = ageMap.indexOf(max);
+  return array[oldestIndex];
 };
 
 // Do not edit below this line
